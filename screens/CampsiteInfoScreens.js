7d8078f61d5419311e5eb47a721f4777df7a897c
Nicole Arroyo <nicole.m.arroyo@gmail.com>
2022-12-08 23:01:55 -0600

    Navigation Part 1

--- screens/CampsiteInfoScreens.js
+++
@@ -1,7 +0,0 @@
-import RenderCampsite from "../features/campsites/RenderCampsite";
-
-const CampsiteInfoScreen = (props) => {
-    return <RenderCampsite campsite={props.campsite} />;
-};
-
-export default CampsiteInfoScreen;
