0ca93599690b85d453270fef388b0314540d1527
Nicole Arroyo <nicole.m.arroyo@gmail.com>
2022-12-31 19:11:22 -0600

    Week 4

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -232,7 +232,18 @@ const Main = () => {
     }, [dispatch]);
 
     useEffect(() => {
-        NetInfo.fetch().then((connectionInfo) => {
+        (showNetInfo)
+        const unsubscribeNetInfo = NetInfo.addEventListener(
+            (connectionInfo) => {
+                handleConnectivityChange(connectionInfo);
+            }
+        );
+        return unsubscribeNetInfo;
+    
+    },[]);
+    
+    const showNetInfo = async () => {   
+        const connectionInfo = await NetInfo.fetch();
             Platform.OS === 'ios'
             ? Alert.alert(
                 'Initial Network Connectivity Type:',
@@ -243,15 +254,7 @@ const Main = () => {
                 connectionInfo.type,
                 ToastAndroid.LONG
             );
-        });
-
-        const unsubscribeNetInfo = NetInfo.addEventListener(
-            (connectionInfo) => {
-                handleConnectivityChange(connectionInfo);
-            }
-        );
-        return unsubscribeNetInfo;
-    }, []);
+    };
 
     const handleConnectivityChange = (connectionInfo) => {
         let connectionMsg = 'You are now connected to an active network.';
